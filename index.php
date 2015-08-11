89ee95c6f39953874a35ab3fcfbda109d2925c99
vova95 <zubenkov95@gmail.com>
2015-08-11 13:46:33 +0300

    first make up

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Landing Page</title>
+	<meta charset="utf-8">
+	<script type="text/javascript" src="http://userapi.com/js/api/openapi.js?34"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=PT+Sans" />
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">
+			<img class="logo" src="img/logo.png"></img>
+			<p class="title">Денежная стена Вконтакте</p>
+			<p class="description">Рекомендуйте шаблоны друзьям и зарабатывайте 1.000.000</p>
+			<img class="mouse" src="img/mouse.png"></img>
+		</div>
+		<div class="content">
+			<div class="advertisement">
+				<p align="center" style="color: #2573b5; font-size: 32px; margin: 0;">Привет!</p>
+				<p>Зарабатывать с <span style="color: #2573b5;">Template</span><span style="color: #de101a;">Monster</span> – мировым производителем шаблонов сайтов не только весело и увлекательно, но еще и очень просто.</p>
+
+				<p>Представляем вашему вниманию инструмент, который позволит мгновенно разослать сообщения с вашей аффилиатской ссылкой по всем вашим друзьям вконтакте.</p>
+
+				<p>Вам останется только проверять статистику и смотреть на то, как растет доход.</p> 
+			</div>
+		</div>
+	</div>
+</body>
+</html>
