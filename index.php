c5919be0b1ba7907bda23cac60b2c655d69e763d
vova95 <zubenkov95@gmail.com>
2015-08-12 08:40:22 +0300

    message

--- index.php
+++ index.php
@@ -17,12 +17,28 @@
 		</div>
 		<div class="content">
 			<div class="advertisement">
-				<p align="center" style="color: #2573b5; font-size: 32px; margin: 0;">Привет!</p>
-				<p>Зарабатывать с <span style="color: #2573b5;">Template</span><span style="color: #de101a;">Monster</span> – мировым производителем шаблонов сайтов не только весело и увлекательно, но еще и очень просто.</p>
+				<h3 align="center" class="heading">Привет!</h3>
+				<p>Зарабатывать с <span style="color: #2573b5; font-weight: bold;">Template</span><span style="color: #de101a; font-weight: bold;">Monster</span> – мировым производителем шаблонов сайтов не только весело и увлекательно, но еще и очень просто.</p>
 
-				<p>Представляем вашему вниманию инструмент, который позволит мгновенно разослать сообщения с вашей аффилиатской ссылкой по всем вашим друзьям вконтакте.</p>
+				<p class="indent">Представляем вашему вниманию инструмент, который позволит мгновенно разослать сообщения с вашей аффилиатской ссылкой по всем вашим друзьям вконтакте.</p>
 
-				<p>Вам останется только проверять статистику и смотреть на то, как растет доход.</p> 
+				<p class="indent">Вам останется только проверять статистику и смотреть на то, как растет доход.</p> 
+			</div>
+			<div class="main_content">
+				<h3 class="heading">Поехали!</h3>
+				<label>
+					Ваш логин в партнерской программе TemplateMonster:
+					<a href="" class="no_login">Нет логина?</a>
+					<input type = "text" class="login"></input>
+				</label>
+				<label>
+					Про какой продукт хотите рассказать:
+					<select type = "text" class="products">
+						<option>Magento</option>
+						<option>PrestaShop</option>
+						<option>Opencart</option>
+					</select>
+				</label>
 			</div>
 		</div>
 	</div>
